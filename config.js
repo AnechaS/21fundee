342048d708632fff5f2145513e46e6049f898f1a
Anechasun <Anachitheu.k@gmail.com>
2020-01-20 22:32:32 +0700

    update confixg to server heroku

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const configPath = './.config.json';
-const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+// const configPath = './.config.json';
+// const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 
-// TODO add env config
+// // TODO add env config
+// exports.mongodb = parsed.mongodb;
+// exports.port = parsed.port;
+
 exports.mongodb = parsed.mongodb;
-exports.port = parsed.port;
+exports.port = process.env.POST || 3000;
