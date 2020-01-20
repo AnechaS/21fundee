5f38bae9ff61f31dc108d1d2c3ff05559def7960
Anechasun <Anachitheu.k@gmail.com>
2020-01-20 22:44:31 +0700

    git update config

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// const configPath = './.config.json';
-// const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+const configPath = './.config.json';
+const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 
-// // TODO add env config
-// exports.mongodb = parsed.mongodb;
-// exports.port = parsed.port;
-
-exports.mongodb = 'mongodb+srv://root:vLwymUb052@clusterx-ciifu.mongodb.net/21fundee';
-exports.port = 3333;
+// TODO add env config
+exports.mongodb = parsed.mongodb;
+exports.port = parsed.port;
