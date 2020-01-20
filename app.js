828e476497bca3ab5ee717d5a174fdf1ba09ecf3
Anechasun <Anachitheu.k@gmail.com>
2020-01-20 22:17:34 +0700

    update config

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const appConfig = require('./config.json');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/people');
+const peopleRouter = require('./routes/people');
 
 mongoose.connect(appConfig.mongodb, {
   useNewUrlParser: true,
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/people', usersRouter);
+app.use('/people', peopleRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
