002cf0633cafda6431911b3d584e5fb0a1b8f13f
Anechasun <Anachitheu.k@gmail.com>
2020-02-05 01:46:34 +0700

    update app.js

--- app.js
+++ app.js
@@ -46,8 +46,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({ 
     message: err.message, 
-    code: err.status, 
-    detail: req.app.get('env') === 'development' ? err : {} 
+    code: err.status
   });
 });
 
