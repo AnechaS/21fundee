9b008c77a55177fc929e884ffe486e00ff539168
Anechasun <Anachitheu.k@gmail.com>
2020-02-21 05:25:52 +0700

    update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use((err, req, res, next) => {
   };
 
   // if (req.app.get('env') !== 'development') {
-  // delete response.stack;
+  delete response.stack;
   // }
 
   res.status(convertedError.status);
