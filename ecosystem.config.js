0947c507db86f362ca74639fc074bd97f6263aa0
Anechasun <Anachitheu.k@gmail.com>
2020-02-25 21:34:43 +0700

    Update ecosystem.congig.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: 'node ./bin/server',
       watch: true,
       ignore_watch: ['node_modules', './client'],
+      max_memory_restart: '500M',
       env: {
         NODE_ENV: 'development'
       },
